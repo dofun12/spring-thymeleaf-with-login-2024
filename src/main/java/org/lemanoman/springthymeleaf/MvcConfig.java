b7594fde3083f8d68d104fef17306b63955c8d6d
kevim <k.v.n.such@gmail.com>
2024-08-23 00:50:07 -0300

    primeiro commit

---
+++ src/main/java/org/lemanoman/springthymeleaf/MvcConfig.java
@@ -0,0 +1,38 @@
+package org.lemanoman.springthymeleaf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+	@Override
+	public void configureContentNegotiation(
+			ContentNegotiationConfigurer configurer) {
+		// Simple strategy: only path extension is taken into account
+		configurer.ignoreAcceptHeader(false).
+				defaultContentType(MediaType.APPLICATION_JSON).
+				mediaType("html", MediaType.valueOf("text/html")).
+				mediaType("css", MediaType.valueOf("text/css")).
+				mediaType("js", MediaType.valueOf("application/javascript"))
+		;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+
+}
--- src/main/java/org/lemanoman/springthymeleaf/SpringThymeleafWithLogin2024Application.java
+++ src/main/java/org/lemanoman/springthymeleaf/SpringThymeleafWithLogin2024Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringThymeleafWithLogin2024Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringThymeleafWithLogin2024Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafWithLogin2024Application.class, args);
+    }
 
 }
