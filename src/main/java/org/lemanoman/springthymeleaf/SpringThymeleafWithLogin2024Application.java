7e101d0977c84a9000c6c9398ec4cdc121b6e111
kevim <k.v.n.such@gmail.com>
2024-08-23 00:46:04 -0300

    Initial commit

---
+++ src/main/java/org/lemanoman/springthymeleaf/SpringThymeleafWithLogin2024Application.java
@@ -0,0 +1,13 @@
+package org.lemanoman.springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafWithLogin2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafWithLogin2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/lemanoman/springthymeleaf/SpringThymeleafWithLogin2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemanoman.springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafWithLogin2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
